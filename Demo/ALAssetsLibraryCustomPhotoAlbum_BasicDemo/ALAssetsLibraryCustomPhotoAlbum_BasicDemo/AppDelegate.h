c67a6c959ce2ff8737980b6b26b1ff4073e50adb
Kjuly <kj@kjuly.com>
2013-01-07 13:41:46 +0800

    Add Demo

---
+++ Demo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ALAssetsLibraryCustomPhotoAlbum_BasicDemo
+//
+//  Created by Kjuly on 1/7/13.
+//  Copyright (c) 2013 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
---
+++ Demo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ALAssetsLibraryCustomPhotoAlbum_BasicDemo
+//
+//  Created by Kjuly on 1/7/13.
+//  Copyright (c) 2013 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
