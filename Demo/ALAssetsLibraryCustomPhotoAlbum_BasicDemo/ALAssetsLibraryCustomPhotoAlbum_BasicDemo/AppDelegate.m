e915ea61b9641b9445fbe1fb7fd0778d685874c2
Kjuly <dev@kjuly.com>
2015-08-30 13:36:49 +0800

    Rm Demo's RootViewController

--- Demo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/AppDelegate.m
+++ Demo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/ALAssetsLibraryCustomPhotoAlbum_BasicDemo/AppDelegate.m
@@ -8,7 +8,8 @@
 
 #import "AppDelegate.h"
 
-#import "RootViewController.h"
+#import "PhotosTableViewController.h"
+
 
 @implementation AppDelegate
 
@@ -16,37 +17,45 @@
 didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  // Override point for customization after application launch.
   
   // Setup root view controller
-  RootViewController * rootViewController;
-  rootViewController = [[RootViewController alloc] init];
-  [self.window setRootViewController:rootViewController];
+  PhotosTableViewController * photosTableViewController = [PhotosTableViewController alloc];
+  (void)[photosTableViewController initWithStyle:UITableViewStylePlain];
+  // Main navigation controller
+  UINavigationController * navigationController = [UINavigationController alloc];
+  (void)[navigationController initWithRootViewController:photosTableViewController];
+  [navigationController.view setFrame:(CGRect){CGPointZero, {kKYViewWidth, kKYViewHeight}}];
+  [self.window setRootViewController:navigationController];
   
   [self.window makeKeyAndVisible];
   [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
   return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application
+{
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
   // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application
+{
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
